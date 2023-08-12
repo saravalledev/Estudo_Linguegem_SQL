908247934b0baa020d1d9523f147ad9249ad145b
Hebert Saravalle <79430844+HebertVS@users.noreply.github.com>
2023-08-11 23:51:47 -0300

    Update SQLQuery7.sql

--- Exercicios_sql/SQLQuery7.sql
+++ Exercicios_sql/SQLQuery7.sql
@@ -1,3 +1,6 @@
+/*Esse script SQL utiliza um cursor para percorrer os resultados de uma consulta em uma visualização chamada vmGOLD. 
+O objetivo é imprimir os valores da coluna fantasia para cada registro da visualização.*/
+
 declare @fantasia  varchar(40)
 -- -----------------------------------
 declare X CURSOR FOR
